3765556912a1bde1c97a819b48c3ef5b8577735f
Denis Blondeel <denis.blondeel@student.ucll.be>
2018-01-15 12:44:25 +0100

    HalfJittered complete

--- raytracer/raytracer/samplers/halfjittered-sampler.cpp
+++ raytracer/raytracer/samplers/halfjittered-sampler.cpp
@@ -16,7 +16,31 @@ namespace
 
 			std::vector<Point2D> sample(const math::Rectangle2D& rectangle)	const override
 			{
+				std::vector<Point2D> result;
+				Rasterizer rast(rectangle, N, M);
 
+				for (int i = 0; i < N; i++)
+				{
+					for (int j = 0; j < M; j++)
+					{
+						Rectangle2D rect = rast[Position2D(N, M)];
+
+						Point2D point = centralizing(rand() / (RAND_MAX + 1.), rand() / (RAND_MAX + 1.));
+
+						result.push_back(rect.from_relative(point));
+					}
+				}
+
+				return result;
+			}
+
+			Point2D centralizing(double x, double y) const
+			{
+				int centralX = x * 0.5 + 0.25;
+				int centralY = y * 0.5 + 0.25;
+
+
+				return Point2D(centralX, centralY);
 			}
 	};
 }
