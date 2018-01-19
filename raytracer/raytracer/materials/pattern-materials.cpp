02472778e039d99f66d48f046c6b3fd830b02921
Denis Blondeel <denis.blondeel@student.ucll.be>
2018-01-19 14:32:30 +0100

    checkered2d implemented

--- raytracer/raytracer/materials/pattern-materials.cpp
+++ raytracer/raytracer/materials/pattern-materials.cpp
@@ -29,3 +29,8 @@ Material raytracer::materials::vertical_lines(double thickness, Material m1, Mat
 {
 	return pattern2d(math::functions::vertical_lines(thickness), m1, m2);
 }
+Material raytracer::materials::checkered2d(Material m1, Material m2)
+{
+	return pattern2d(math::functions::checkered2d(), m1, m2);
+}
+
