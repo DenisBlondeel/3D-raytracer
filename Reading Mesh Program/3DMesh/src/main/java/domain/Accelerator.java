75414fd748d67cb274a3b7925d4639629388e727
Denis Blondeel <denis@wina.be>
2018-08-28 13:43:40 +0200

    Implemented optimiser ply files

---
+++ Reading Mesh Program/3DMesh/src/main/java/domain/Accelerator.java
@@ -0,0 +1,109 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Accelerator {
+	
+	private FaceDB fdb;
+	private List<Box> boxList;
+	
+	public Accelerator(FaceDB fdb)
+	{
+		this.fdb = fdb;
+		optimize(5);
+	}
+	
+	public Vertex findSmallest(FaceDB db)
+	{
+		double x=0;
+		double y=0;
+		double z=0; 
+		
+		for(int i = 0; i < db.getFaces().size(); i++)
+		{
+			Face face = db.getFaces().get(i);
+			if(face.getV1().getX() < x) x=face.getV1().getX();
+			if(face.getV2().getX() < x) x=face.getV2().getX();
+			if(face.getV3().getX() < x) x=face.getV3().getX();
+			
+			if(face.getV1().getY() < y) y=face.getV1().getY();
+			if(face.getV2().getY() < y) y=face.getV2().getY();
+			if(face.getV3().getY() < y) y=face.getV3().getY();
+			
+			if(face.getV1().getZ() < z) z=face.getV1().getZ();
+			if(face.getV2().getZ() < z) z=face.getV2().getZ();
+			if(face.getV3().getZ() < z) z=face.getV3().getZ();
+			
+		}
+		
+		return new Vertex(0,x,y,z);
+	}
+	
+	public Vertex findBiggest(FaceDB db)
+	{
+		double x=0;
+		double y=0;
+		double z=0; 
+		
+		for(int i = 0; i < db.getFaces().size(); i++)
+		{
+			Face face = db.getFaces().get(i);
+			if(face.getV1().getX() > x) x=face.getV1().getX();
+			if(face.getV2().getX() > x) x=face.getV2().getX();
+			if(face.getV3().getX() > x) x=face.getV3().getX();
+			
+			if(face.getV1().getY() > y) y=face.getV1().getY();
+			if(face.getV2().getY() > y) y=face.getV2().getY();
+			if(face.getV3().getY() > y) y=face.getV3().getY();
+			
+			if(face.getV1().getZ() > z) z=face.getV1().getZ();
+			if(face.getV2().getZ() > z) z=face.getV2().getZ();
+			if(face.getV3().getZ() > z) z=face.getV3().getZ();
+		}
+		
+		return new Vertex(0,x,y,z);
+	}
+	
+	public List<Box> getEndNodes()
+	{
+		List<Box> leaves = new ArrayList<Box>();
+		
+		for(Box box : boxList)
+		{
+			if(!box.hasChilden())
+			{
+				leaves.add(box);
+			}
+		}
+		return leaves;
+	}
+	
+	private void optimize(int depth)
+	{
+		Box uberBox = new Box(findSmallest(fdb), findBiggest(fdb),0);
+		
+		boxList = new ArrayList<Box>();
+		
+		
+		boxList.add(uberBox);
+		
+		uberBox.setFaceDB(fdb);		
+		for(int i = 0; i <= depth; i++)
+		{
+			List<Box> temp = new ArrayList<Box>();
+			for(Box box : boxList)
+			{
+				if(!box.hasChilden())
+				{
+					box.split();
+					temp.add(box.getSubBox("left"));
+					temp.add(box.getSubBox("right"));
+				}
+			}
+			boxList.addAll(temp);
+		}
+		System.out.println("There are : " + boxList.size() + " boxes");
+	}
+
+}
---
+++ Reading Mesh Program/3DMesh/src/main/java/domain/Box.java
@@ -0,0 +1,148 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
+
+public class Box {
+	
+	private Vertex begin, end;
+	
+	private FaceDB fdb;
+	
+	private VertexDB db;
+	
+	private int depth = 0;
+	
+	private boolean hasChildren = false;
+	
+	private Box boxL;
+	private Box boxR;
+
+	public Box(Vertex begin, Vertex end, int depth)
+	{
+		this.begin = begin;
+		this.end = end;
+		this.depth = depth;
+	}
+	
+	public Vertex getBegin()
+	{
+		return begin;
+	}
+	
+	public List<Box> getSubBoxes()
+	{
+		List<Box> subBoxes = new ArrayList<Box>();
+		subBoxes.add(boxL);
+		subBoxes.add(boxR);
+		return subBoxes;
+	}
+	
+	public Vertex getEnd() 
+	{
+		return end;
+	}
+	
+	public Box getSubBox(String direction)
+	{
+		if(direction.equals("left"))
+		{
+			return boxL;
+		}
+		else if(direction.equals("right"))
+		{
+			return boxR;
+		}
+		else return null;
+	}
+	
+	public boolean hasChilden()
+	{
+		return hasChildren;
+	}
+	
+	public VertexDB getDb()
+	{
+		return db;
+	}
+	public void setDepth(int depth)
+	{
+		this.depth = depth;
+	}
+	
+	public int getDepth()
+	{
+		return depth;
+	}
+	
+	
+	public void setFaceDB(FaceDB db)
+	{
+		fdb = db;
+	}
+	
+	public FaceDB getTriangleCollection()
+	{
+		return fdb;
+	}
+	
+	public void split()
+	{
+		Vertex newEnd = new Vertex(0, end.getX()/2, end.getY(), end.getZ());
+		boxL = new Box(begin, newEnd, depth);
+		boxL.setFaceDB(facesInBox(boxL, fdb));
+		
+		Vertex newBegin = new Vertex(0, end.getX()/2, begin.getY(), begin.getZ());
+		boxR = new Box(newBegin, end, depth);
+		boxR.setFaceDB(facesInBox(boxR, fdb));
+		
+		hasChildren = true;
+	}
+	
+	private FaceDB facesInBox(Box box, FaceDB db)
+	{
+		FaceDB newFaceDB = new FaceDB();
+		for(int i=0; i < db.getFaces().size(); i++)
+		{
+			if(contains(box, db.getFaces().get(i))) 
+			{	newFaceDB.addFace(db.getFaces().get(i));
+			}
+		}
+		return newFaceDB;
+	}
+	
+	private boolean contains(Box box, Face face)
+	{
+		Vertex fV1 = face.getV1();
+		Vertex fV2 = face.getV2();
+		Vertex fV3 = face.getV3();
+		if(fV1.getX() >= box.getBegin().getX() && fV1.getX() <= box.getEnd().getX() && fV1.getY() >= box.getBegin().getY() && fV1.getY() <= box.getEnd().getY() && fV1.getZ() >= box.getBegin().getZ() && fV1.getZ() <= box.getEnd().getZ())
+		{
+			return true;
+		}
+		
+		else if(fV2.getX() >= box.getBegin().getX() && fV2.getX() <= box.getEnd().getX() && fV2.getY() >= box.getBegin().getY() && fV2.getY() <= box.getEnd().getY() && fV2.getZ() >= box.getBegin().getZ() && fV2.getZ() <= box.getEnd().getZ())
+		{
+			return true;
+		}
+		
+		else if(fV3.getX() >= box.getBegin().getX() && fV3.getX() <= box.getEnd().getX() && fV3.getY() >= box.getBegin().getY() && fV3.getY() <= box.getEnd().getY() && fV3.getZ() >= box.getBegin().getZ() && fV3.getZ() <= box.getEnd().getZ())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public JSONObject toJSON()
+	{
+		JSONObject result = new JSONObject();
+		result.put("v1", begin.toJSON());
+		result.put("v2", end.toJSON());
+		result.put("triangles", fdb.getFaces());
+		return result;
+	}
+}
